d9909407e38f582c8c587ec430e7d97d77db900d
Ryuya Hayashi <ryuya.hayashi.1018@gmail.com>
2020-06-26 21:01:47 +0900

    learn UnionTree

--- ABC/157/d.cpp
+++ ABC/157/d.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
-#include <queue>
+#include <set>
 using namespace std;
 
 typedef long long ll;
@@ -10,52 +10,72 @@ typedef vector<int> vi;
 
 const double PI=acos(-1.0);
 
-int N,M,K; vi F[100010],B[100010];
+struct UnionFind {
+    vi par,siz;
+
+    // UnionFind(int N) : par(N) {
+    //     for(int i = 0; i < N; i++) par[i] = i;
+    // }
+
+    void assign(int N, int n){
+        par.resize(N); siz.resize(N,1);
+        for(int i = 0; i < N; i++) par[i] = n;
+    }
+
+    int root(int x) { 
+        if (par[x]<0) return x;
+        return par[x] = root(par[x]);
+    }
+
+    void unite(int x, int y) {
+        int rx = root(x); 
+        int ry = root(y); 
+        if (rx == ry) return; 
+        if(siz[rx]<siz[ry]) swap(rx,ry);
+        par[ry]=rx;
+        siz[rx]+=siz[ry];
+    }
+
+    bool same(int x, int y) { 
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+
+    int size(int x) {
+        return siz[root(x)];
+    }
+};
+
+int N,M,K; vector<set<int>> rmv; UnionFind uf;
 void input()
 {
     cin>>N>>M>>K;
+    rmv.resize(N);
+    uf.assign(N,-1);
     for (int i = 0; i < M; ++i) {
         int a, b;
-        cin >> a >> b;
-        F[a].push_back(b);
-        F[b].push_back(a);
+        cin>>a>>b;
+        --a; --b;
+        rmv[a].insert(b);
+        rmv[b].insert(a);
+        uf.unite(a,b);
     }
     for (int i = 0; i < K; ++i) {
         int a, b;
-        cin >> a >> b;
-        B[a].push_back(b);
-        B[b].push_back(a);
+        cin>>a>>b;
+        --a; --b;
+        if(!uf.same(a,b)) continue;
+        rmv[a].insert(b);
+        rmv[b].insert(a);
     }
 }
 
 void solve()
 {
-    vi dist;
-    queue<int> que;
-
-    for(int i=1; i<N+1; ++i){
-        dist.assign(N+1,-1);
-        dist[i] = 0;
-        que.push(i);
-        while(!que.empty()) {
-            int v = que.front(); 
-            que.pop();
-            for(int nv: F[v]){
-                if(dist[nv]!=-1) continue; 
-                dist[nv]=dist[v]+1;
-                que.push(nv);
-            }
-        }
-        int ans=0;
-        for(int v=1; v<N+1; ++v){
-            if(dist[v]>1){
-                bool flag=true;
-                for(int nv:B[i]){
-                    if(nv==v) flag=false;
-                }
-                if(flag) ++ans;
-            }
-        }
+    for(int i=0;i<N;++i){
+        int ans=uf.size(i)-rmv[i].size()-1;
+        // cout<<uf.size(i)<<" "<<rmv[i].size()<<" "<<endl;
         cout<<ans<<" ";
     }
     cout<<endl;
