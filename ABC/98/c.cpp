c314813a2ffc16903685fe468cefd8eec7a5ec12
Ryuya Hayashi <ryuya.hayashi.1018@gmail.com>
2020-06-27 21:00:42 +0900

    c

--- ABC/98/c.cpp
+++ ABC/98/c.cpp
@@ -7,17 +7,32 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 
-const double PI=acos(-1.0);
-
-
+int N; string S; vi l,r;
 void input()
 {
-    
+    cin>>N>>S;
 }
 
 void solve()
 {
-    
+    int count=0;
+    for(int i=0;i<N;++i) {
+        if(S[i]=='W') ++count;
+        l.emplace_back(count);
+    }
+    count=0;
+    for(int i=0;i<N;++i) {
+        if(S[N-1-i]=='E') ++count;
+        r.emplace_back(count);
+    }
+    int MIN=100000000;
+    for(int i=0;i<N;++i) {
+        int tmp=0;
+        if(i>0) tmp+=l[i-1];
+        if(i<N-1) tmp+=r[N-2-i];
+        MIN=min(MIN,tmp);
+    }
+    cout<<MIN<<endl;
 }
 
 int main()
