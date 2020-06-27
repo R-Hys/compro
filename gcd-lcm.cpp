48e957220fca2ba144050edcdb9fcf134d64b48f
Ryuya Hayashi <ryuya.hayashi.1018@gmail.com>
2020-06-27 11:47:32 +0900

    gcd template

---
+++ gcd-lcm.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int c=a%b;
+    while(c!=0){
+        a=b; b=c; c=a%b;
+    }
+    return b;
+}
+
+int lcm(int a, int b){
+    return a*b/gcd(a,b);
+}
+
+
+void input()
+{
+    
+}
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    cin.tie();
+    ios::sync_with_stdio(false);
+    input();
+    solve();
+    return 0;
+}
