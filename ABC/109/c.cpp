641b524635f3b7781ad3ed36d5eb23d69ac68547
Ryuya Hayashi <ryuya.hayashi.1018@gmail.com>
2020-06-27 11:58:16 +0900

    109 c

--- ABC/109/c.cpp
+++ ABC/109/c.cpp
@@ -9,15 +9,33 @@ typedef vector<int> vi;
 
 const double PI=acos(-1.0);
 
+int N,X; vi d;
+
+int gcd(int a, int b)
+{
+    int c=a%b;
+    while(c!=0){
+        a=b; b=c; c=a%b;
+    }
+    return b;
+}
 
 void input()
 {
-    
+    cin>>N>>X;
+    for(int i=0;i<N;++i){
+        int a; cin>>a; 
+        d.emplace_back((X-a>0)?X-a:a-X);
+    }   
 }
 
 void solve()
 {
-    
+    int ans=d[0];
+    for(int i=1; i<d.size(); ++i){
+        ans=gcd(ans,d[i]);
+    }
+    cout<<ans<<endl;
 }
 
 int main()
