0828bb3f0d30fd2273734b3a4d8da9864a2e26b7
Ryuya Hayashi <ryuya.hayashi.1018@gmail.com>
2020-08-06 20:19:14 +0900

    fill C

--- ABC/108/c.cpp
+++ ABC/108/c.cpp
@@ -5,19 +5,34 @@
 using namespace std;
 
 typedef long long ll;
-typedef vector<int> vi;
 
-const double PI=acos(-1.0);
-
-
+ll N,K;
 void input()
 {
-    
+    cin >> N >> K;
 }
 
 void solve()
 {
-    
+    ll a,b,c; 
+    ll ans = 0;
+    for(a = 1; a <= K; ++a){
+        b = c = K - a % K;
+        ll a_cnt = 0, b_cnt = 0;
+        if((b+c) % K == 0){
+            ll tmp_a = a;
+            while(a <= N){
+                a += K; ++a_cnt;
+            }
+            while(b <= N){
+                b += K; ++b_cnt;
+            }
+            ans += a_cnt * b_cnt * b_cnt;
+            a = tmp_a;
+            // cout << a_cnt << " " << b_cnt << endl;
+        }
+    }
+    cout << ans << endl;
 }
 
 int main()
